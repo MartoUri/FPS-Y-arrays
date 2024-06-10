2e06cd2ef3b3844b07dcceb95d76ac0496699797
Martin <martin71211@gmail.com>
2024-06-10 14:25:18 -0300

    Initial commit

---
+++ Assets/InteractionArea.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionArea : MonoBehaviour
+{
+    public GameObject filete;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Colision");
+        if (collision.gameObject == filete) Debug.Log("Estaba rico el filete");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Colision con" + other.name);
+    }
+}
